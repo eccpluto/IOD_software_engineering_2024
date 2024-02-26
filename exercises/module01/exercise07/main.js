e45902f0ae34ff1815c8f59c2f02d96cc2af9a1f
eccpluto <alleyes.payday591@passmail.net>
2024-02-26 20:32:02 +1300

    exercise07 complete - BUT would be good to ask about divide by zero error handling / testing for expected error handling

--- exercises/module01/exercise07/main.js
+++ exercises/module01/exercise07/main.js
@@ -8,7 +8,7 @@ function add(a, b) {
     return a + b;
 }
 
-// TODO this function returns
+// this function returns the result of dividing the first argument into the second
 function divide(a, b) {
     if (b==0) {
         throw new Error("Cannot divide by zero.");
@@ -16,10 +16,42 @@ function divide(a, b) {
     return a / b;
 }
 
+// this function returns the result of multiplying the two arguments together
 function multiply(a, b) {
     return a * b;
 }
 
+// this function with greet the person via the console, given a name argument
 function say_hello(name) {
     console.log("Hello " + name);
 }
+
+// testing our functions
+
+// subtraction
+if (subtract(0,0) != 0) { throw new Error };
+if (subtract(0,1) != -1) { throw new Error };
+if (subtract(1,0) != 1) { throw new Error };
+if (subtract(1,1) != 0) { throw new Error };
+
+// add
+if (add(0,0) != 0) { throw new Error };
+if (add(-1,1) != 0) { throw new Error };
+if (add(-2,-3) != -5) { throw new Error };
+if (add(2,3) != 5) { throw new Error };
+
+
+// divide
+if (divide(1, 1) != 1) { throw new Error };
+if (divide(0, 99999) != 0) { throw new Error };
+if (divide(1, -1) != -1) { throw new Error };
+
+
+// console.log(divide(10,3));
+// if (divide(10, 3) != 1){ throw new Error };
+// if (divide(1, 0) != error) { error };
+
+if (multiply(1,1) != 1) { throw new Error };
+if (multiply(0,1) != 0) { throw new Error };
+if (multiply(10,-1) != -10) { throw new Error };
+if (multiply(-1,-1) != 1) { throw new Error };
