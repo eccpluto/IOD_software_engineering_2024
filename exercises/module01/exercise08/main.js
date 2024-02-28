bae38f5da38b3bb766a672ee6919a3e70576c7ca
eccpluto <alleyes.payday591@passmail.net>
2024-02-28 18:39:04 +1300

    implemented a function that returns the number for rolling a dice

---
+++ exercises/module01/exercise08/main.js
@@ -0,0 +1,12 @@
+/**
+ * Simulates rolling a dice, given a faces value, which is the
+ * number of sides to use, and returns a number.
+ * @param {number} faces 
+ * @returns {number}
+ */
+
+function getDiceRoll(faces) {
+
+    // get a unmber between 1 and the number of faces
+    return Math.floor(Math.random() * faces) + 1;
+}
