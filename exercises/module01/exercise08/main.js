b09e7b7ac58cae92cbf6bae75056ed626439ee72
eccpluto <rendar@pm.me>
2024-03-06 22:42:30 +1300

    added dice logic for exercise08

--- exercises/module01/exercise08/main.js
+++ exercises/module01/exercise08/main.js
@@ -5,8 +5,28 @@
  * @returns {number}
  */
 
+let g_faces = 6; // D6 as default dice type / number of faces
+
 function getDiceRoll(faces) {
 
     // get a unmber between 1 and the number of faces
     return Math.floor(Math.random() * faces) + 1;
+}
+
+/**
+ * Updates the dice type which is determined by the number of faces on a die
+ * For example, faces == 6 is a D6, faces == 10 is a D10 
+ * @param {number} faces 
+ */
+function updateDiceType() {
+    let faces = Number(document.getElementById('textInputDiceType').value);
+    // only update dice type if the user submits a number, and is different to the current value
+    if( !isNaN(faces) && faces != g_faces) {
+        g_faces = faces;
+    }
+}
+
+function printDiceRoll() {
+    let test = getDiceRoll();
+    document.getElementById("diceNumber").innerHTML = test;
 }
