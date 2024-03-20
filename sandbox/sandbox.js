32c1acbbd07a1478b8b2bfea2b065d5fc8ef4891
eccpluto <rendar@pm.me>
2024-03-20 19:32:07 +1300

    added template source for lab2.3, played around in sandbox

--- sandbox/sandbox.js
+++ sandbox/sandbox.js
@@ -1,3 +1,10 @@
-let someVar = "hello";
-someVar = Number(someVar);
-console.log(someVar)
+// let someVar = "hello";
+// someVar = Number(someVar);
+// console.log(someVar)
+
+let float1 = 0.1;
+let float2 = 0.2;
+console.log(float1.toPrecision(18));
+console.log(float2.toPrecision(18));
+
+console.log(float1 + float2);
