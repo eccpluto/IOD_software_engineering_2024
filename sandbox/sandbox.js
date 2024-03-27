3c30214063de65c5eb6e1079d20ef7d307094e80
eccpluto <rendar@pm.me>
2024-03-27 21:17:37 +1300

    sandbox stuff - investigating timed synchronous calls

--- sandbox/sandbox.js
+++ sandbox/sandbox.js
@@ -2,9 +2,42 @@
 // someVar = Number(someVar);
 // console.log(someVar)
 
-let float1 = 0.1;
-let float2 = 0.2;
-console.log(float1.toPrecision(18));
-console.log(float2.toPrecision(18));
+// let float1 = 0.1;
+// let float2 = 0.2;
+// console.log(float1.toPrecision(18));
+// console.log(float2.toPrecision(18));
 
-console.log(float1 + float2);
+// console.log(float1 + float2);
+
+
+function PrintMe(msg) {
+    console.log(msg);
+}
+
+let timerID = setTimeout(PrintMe, 1, "Asynchronously printed message");
+
+let time = performance.now()
+
+console.log("synchronous code");
+console.log("synchronous code");
+console.log("synchronous code");
+console.log("synchronous code");
+console.log("synchronous code");
+console.log("synchronous code");
+console.log("synchronous code");
+console.log("synchronous code");
+console.log("synchronous code");
+console.log("synchronous code");
+console.log("synchronous code");
+console.log("synchronous code");
+console.log("synchronous code");
+console.log("synchronous code");
+console.log("synchronous code");
+
+
+
+console.log(`time taken: ${performance.now() - time}`) 
+
+/* (DR) time taken prints ~5 milliseconds, but asynchronous timer was 1 ms..
+ * this indicates setTimout delay is not started until the synchronous calls above execute
+ */
