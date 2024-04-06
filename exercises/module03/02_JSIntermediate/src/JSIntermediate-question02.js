851235b556665b521c3afe4cfcae3307a34db8de
eccpluto <rendar@pm.me>
2024-04-06 13:54:17 +1300

    question 02 commit

--- exercises/module03/02_JSIntermediate/src/JSIntermediate-question02.js
+++ exercises/module03/02_JSIntermediate/src/JSIntermediate-question02.js
@@ -9,17 +9,47 @@
  * // This text will be truncat...
  */
 
+/**
+ * 
+ * @param {string} str 
+ * @param {number} max 
+ * @returns A truncated version of the given string, limitted to the max length provided.
+ */
 function truncate(str, max) {
     // console.log((String(str).length) <= max);
-    // let String(str).length = strLength;
-    if (String(str).length <= max) { return str };
+    let strLength = String(str).length;
+    if (strLength <= max) { return str };
     
     // otherwise proceed to truncate by returning a substring
     // not although zero-indexed, the character at the terminating index is not included - so we dont need "max - 1"
     let truncatedStr = String(str).substring(0, max);
-    console.log(truncatedStr);
 
-    return (String(str).length > max) ? truncatedStr + "..." : truncatedStr;
+    if (strLength > max) {
+        return truncatedStr + '...';
+    } else {
+        return truncatedStr;
+    }
 }
 
-console.log(truncate("", 0));
+console.log(truncate('This text will be truncated if it is too long', 25));
+
+/**
+ * 
+ * @param {string} str 
+ * @param {number} max 
+ * @returns A truncated version of the given string, limitted to the max length provided. Internally uses conditional operator.
+ */
+function truncateVariant(str, max) {
+    // console.log((String(str).length) <= max);
+    let strLength = String(str).length;
+    if (strLength <= max) { return str };
+    
+    // otherwise proceed to truncate by returning a substring
+    // not although zero-indexed, the character at the terminating index is not included - so we dont need "max - 1"
+    let truncatedStr = String(str).substring(0, max);
+
+    // conditional operator variation
+    return (strLength > max) ? truncatedStr + "..." : truncatedStr;
+}
+
+console.log(truncateVariant('This text will be truncated by the conditional operator variant function, if it is too long', 80));
