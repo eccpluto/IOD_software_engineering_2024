0fc1f43efff3e8aaa654c15b2543969cba799bad
eccpluto <rendar@pm.me>
2024-04-17 21:46:42 +1200

    starting JSAdvanced question 06

---
+++ exercises/module03/03_JSAdvanced/src/JSAdvanced-question06.js
@@ -0,0 +1,16 @@
+/**
+ * Use the ```Function``` prototype to add a new ```delay(ms)``` function to all functions, which can
+ * be used to delay the call to that function by ```ms``` milliseconds
+ */
+
+
+// console.log(Object.getOwnPropertyNames(Object.getPrototypeOf(Object)));
+
+
+// console.log(Function.prototype);
+
+// function multiply(a, b) {
+//     console.log(a * b);
+// }
+
+// multiply.delay(500)(5, 5); // prints 25 after 500ms
