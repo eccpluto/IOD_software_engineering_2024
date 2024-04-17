27f7e8c309c9d7bdeb94ea29aa3c2d3293bd0099
eccpluto <alleyes.payday591@passmail.net>
2024-04-17 20:49:39 +1200

    fixed JSAdvanced question 03, started 05

--- exercises/module03/03_JSAdvanced/src/JSAdvanced-question03.js
+++ exercises/module03/03_JSAdvanced/src/JSAdvanced-question03.js
@@ -4,25 +4,49 @@
  * as page scrolling in a browser.
  */
 
-function printMe() {
-    console.log('printing debounced message')
+function printMe(msg = 'printing debounced message') {
+    console.log(msg)
 }
 
-// need to restart an internal cooldown timer each time the debounced function is called
-function debounce(func) {
-    // delcare a coolDown variable, which will become a timer when the returned function is called
-    let coolDown;
+// a) 
+/**
+ * 
+ * @param {Function} func to have debouncing behaviour applied.
+ * @param {Number} ms time in milliseconds to debounce between successive calls to func. Defaults to 1000 (1 second).
+ * @returns callable function that will debounce with a wait time of 1 second.
+ */
+function debounce(func, ms = 1000) {
+    // delcare a coolDownTimer which will allow us to store time since last call to the debounced function
+    let coolDownTimer;
     return function () {
-        clearTimeout(coolDown);
-        // initialise the coolDown timer
-        coolDown = setTimeout(() => func.apply(this, arguments));
+        // re-init the cool down timer
+        clearTimeout(coolDownTimer);
+        // re-call the decorated function after a 1 second delay
+        coolDownTimer = setTimeout(() => func.apply(this, arguments), ms);
     }
 }
 
-// note this is defefining, not redeclaring hence no variable qualifier (const, let, var)
-printMe = debounce(printMe); //create this debounce function for a)
+// TEST ME
+// printMe = debounce(printMe); //create this debounce function for a)
+// // fire off 3 calls to printMe within 300ms - only the LAST one should print, after 1000ms of no calls
+// setTimeout(printMe, 100); // coolDownTimer reset -> printMe will call after ~1100ms
+// setTimeout(printMe, 200); // coolDownTimer reset (disables previous call) -> printMe will execute after ~1200ms
+// setTimeout(printMe, 300); // coolDownTimer reset (disables previous call) -> printMe will execute after ~1300ms
 
-// fire off 3 calls to printMe within 300ms - only the LAST one should print, after 1000ms of no calls
-setTimeout(printMe, 100);
-setTimeout(printMe, 200);
-setTimeout(printMe, 300);
+// b) Extend the debounce decorator function above to take a second argument ms, which defines the length
+// of the period of inactivity instead of hardcoding to 1000ms - See above.
+
+// TEST ME
+// printMe = debounce(printMe, 150);
+// setTimeout(printMe, 200); // coolDownTimer reset -> printMe will execute after ~350ms
+// setTimeout(printMe, 300); // coolDownTimer reset (disables previous call) -> printMe will execute after ~450ms
+// setTimeout(printMe, 800); // coolDownTimer reset -> printMe will execute after ~950ms
+
+// c) Extend debounce to allow the original debounced function printMe to take an argument msg which is
+// included in the console.log statement - See original function.
+
+// TEST ME
+// printMe = debounce(printMe, 1000);
+// setTimeout(printMe, 100, 'timeout set to 100ms'); // coolDownTimer reset -> printMe will call after ~1100ms
+// setTimeout(printMe, 200, 'timeout set to 200ms'); // coolDownTimer reset (disables previous call) -> printMe will execute after ~1200ms
+// setTimeout(printMe, 300, 'timeout set to 300ms'); // coolDownTimer reset (disables previous call) -> printMe will execute after ~1300ms
---
+++ exercises/module03/03_JSAdvanced/src/JSAdvanced-question05.js
@@ -0,0 +1,5 @@
+/**
+ * The following car object has several properties and a method which uses them to print a
+ * description. When calling the function normally this works as expected, but using it from
+ * within setTimeout fails. Why?
+ */
