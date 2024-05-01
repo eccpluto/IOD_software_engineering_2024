028ae2ddd474c4ade6ff196a59c1c5f6fc7f2412
eccpluto <rendar@pm.me>
2024-05-02 00:12:49 +1200

    groundwork and some event handling for calculator buttons

---
+++ exercises/module04/lab4.3/src/main.js
@@ -0,0 +1,20 @@
+/**
+ * Calculator script
+ */
+
+// Core calculator functions
+function Add(x, y) {
+    return x + y;
+}
+
+function Subtract(x, y) {
+    return x - y;
+}
+
+function Multiply(x, y) {
+    return x * y;
+}
+
+function Divide(x, y) {
+    return x / y;
+}
