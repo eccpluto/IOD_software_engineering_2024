309bee8a422e2c0d8a24988a92835474a0a770b0
eccpluto <alleyes.payday591@passmail.net>
2024-05-04 14:40:19 +1200

    completed lab5.2

--- exercises/module05/sandbox/server-side-calculator/routes/calculatorRoutes.js
+++ exercises/module05/sandbox/server-side-calculator/routes/calculatorRoutes.js
@@ -2,8 +2,17 @@ const express = require("express");
 const router = express.Router();
 
 router.get('/add', (req, res) => {
-    console.log(req.query);
-    res.send(req.query);
+    // console.log(req.query);
+    // res.send(req.query);
+
+    let number1 = parseInt(req.query.num1);
+    let number2 = parseInt(req.query.num2);
+    let sum = number1 + number2;
+    console.log(sum);
+    res.status(200);
+    res.json({
+        result: sum
+    });
 });
 
 module.exports = router;
