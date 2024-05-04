f9141935e60f12e5541225e9797d5afbcccff185
eccpluto <alleyes.payday591@passmail.net>
2024-05-05 11:13:30 +1200

    completed parts 1 throught 4 lab 5.4

--- exercises/module05/lab5.4/m5lab4_expressapp/routes/friendRoutes.js
+++ exercises/module05/lab5.4/m5lab4_expressapp/routes/friendRoutes.js
@@ -24,19 +24,27 @@ router.get('/', (req, res) => {
 router.get('/filter', (req, res) => {
     console.log(req.query)
     let filterGender = req.query.gender;
+    let filterLetter = req.query.letter.toUpperCase();
     let matchingFriends = [...friends];
 
     if (filterGender) {
         matchingFriends = matchingFriends.filter(friend => friend.gender == filterGender);
     }
-    
+
+    if (filterLetter) {
+        matchingFriends = matchingFriends.filter(friend => {
+            let name = friend.name.toUpperCase();
+            return name.startsWith(filterLetter);
+        });
+    }
+
     if (matchingFriends.length > 0) {
         // return valid data when the gender matches 
         res.status(200).json(matchingFriends)
     } else {
         // and an error response when there are no matches
-        res.status(404).json({error: "No friends matching gender "+filterGender})
-    }  
+        res.status(404).json({ error: `No friends matching gender ${filterGender}, starting with "${filterLetter}"` })
+    }
 })
 
 // 2. Get information about this request from the headers
@@ -44,7 +52,7 @@ router.get('/info', (req, res) => {
     console.log(req.headers)
 
     // Modify this response to just return info on the user-agent, content-type and accept headers
-    res.json(req.headers)  
+    res.json({ 'User-Agent': req.headers['user-agent'], 'Content-Type': 'application/json', Accept: req.headers.accept });
 })
 
 // 3. Dynamic request param endpoint - get the friend matching the specific ID ie. /friends/3
@@ -52,10 +60,20 @@ router.get('/:id', (req, res) => {
     console.log(req.params)
     let friendId = req.params.id; // 'id' here will be a value matching anything after the / in the request path
 
+    console.log(friendId);
+
     // Modify this function to find and return the friend matching the given ID, or a 404 if not found
+    let foundFriend = [...friends];
+    foundFriend = foundFriend.find(friend => friend.id == friendId);
+
+    if (foundFriend) {
+        res.status(200).json(foundFriend);
+    } else {
+        res.status(404).json({ error: `Friend with id: ${friendId} not found` });
+    }
 
     // Modify this response with the matched friend, or a 404 if not found
-    res.json({result: 'Finding friend with ID ' + friendId})
+    // res.json({ result: 'Finding friend with ID ' + friendId })
 })
 
 // a POST request with data sent in the body of the request, representing a new friend to add to our list
@@ -65,7 +83,7 @@ router.post('/', (req, res) => {
 
     // we can add some validation here to make sure the new friend object matches the right pattern
     if (!newFriend.name || !newFriend.gender) {
-        res.status(500).json({error: 'Friend object must contain a name and gender'});
+        res.status(500).json({ error: 'Friend object must contain a name and gender' });
         return;
     }
     else if (!newFriend.id) {
@@ -81,11 +99,19 @@ router.post('/', (req, res) => {
 router.put('/:id', (req, res) => {
     let friendId = req.params.id;
     let updatedFriend = req.body;
+    console.log(updatedFriend);
 
     // Replace the old friend data for friendId with the new data from updatedFriend
-
-    // Modify this response with the updated friend, or a 404 if not found
-    res.json({result: 'Updated friend with ID ' + friendId, data: updatedFriend})
+    let friendIndex = friends.findIndex(friend => friend.id == friendId);
+    if (friendIndex != -1) {
+        // only update SPECIFIC property values if they are provided in the request body.
+        // We ignore requests to update the Id.
+        if (updatedFriend.name) { friends[friendIndex].name = updatedFriend.name };
+        if (updatedFriend.gender) { friends[friendIndex].gender = updatedFriend.gender };
+        res.status(200).json({ result: 'Updated friend with ID ' + friendId, data: friends[friendIndex] })
+    } else {
+        res.status(404).json({ result: "Cound not find friend with ID " + friendId });
+    }
 })
 
 module.exports = router;
