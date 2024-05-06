cfb11bf6899e1aa1c45f35d794371861b3fb1a6d
eccpluto <rendar@pm.me>
2024-05-06 19:44:40 +1200

    testing skeletons - will not work with the current controller code -- these would require mocking requestr and response headers. WIll make libraries before implementing test

---
+++ exercises/module05/lab5.3&5.5-5.7/tests/add.test.js
@@ -0,0 +1,15 @@
+// const calculatorController = require('../controllers/calculatorController');
+// const controller = new calculatorController();
+
+// test('add 1 to -1 to get 0', () => {
+//     expect(controller.add({
+//         query: {
+//             num1: "1",
+//             num2: '-1'
+//         }
+//     }, {result: {sum: ''}})).toBe({
+//         result: {
+//             sum: "0"
+//         }
+//     })
+// })
