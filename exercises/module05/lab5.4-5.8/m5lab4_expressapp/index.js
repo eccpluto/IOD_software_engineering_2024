0e1eaa2d0cf459095247818ac4e3569a00242bbc
eccpluto <rendar@pm.me>
2024-05-06 18:10:52 +1200

    working on swagger docs exercise 8

--- exercises/module05/lab5.4-5.8/m5lab4_expressapp/index.js
+++ exercises/module05/lab5.4-5.8/m5lab4_expressapp/index.js
@@ -1,14 +1,24 @@
 const express = require("express"); // import the express package
 const friendRoutes = require('./routes/friendRoutes');
+const swaggerUI = require("swagger-ui-express");
+const swaggerDocument = require('./swagger.json');
 
 const app = express(); // create a new app
 const port = 3000; // change this to run the app on a different port - usually a 4 digit number
+
+// swagger setup
+app.use(
+  '/api-docs',
+  swaggerUI.serve,
+  swaggerUI.setup(swaggerDocument)
+)
 
 // parse requests of content-type - application/json (needed for POST and PUT requests using req.body)
 app.use(express.json());
 
 app.use('/', express.static('public'))
 app.use('/friends', friendRoutes);
+
 
 // starts the backend app on the given port
 app.listen(port, () => {
