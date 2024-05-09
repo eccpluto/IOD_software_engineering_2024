c8d17efd982855a0ea1ff85fcef414e5de271ff0
eccpluto <rendar@pm.me>
2024-05-09 23:24:23 +1200

    extracted ccontroller class functions into file, fixed potentially broken logic

--- exercises/module05/lab5.3&5.5-5.7/controllers/calculatorController.js
+++ exercises/module05/lab5.3&5.5-5.7/controllers/calculatorController.js
@@ -1,49 +1,45 @@
-class calculatorController {
-    constructor() { };
+function add(req, res) {
+    let number1 = parseInt(req.query.num1);
+    let number2 = parseInt(req.query.num2);
+    let sum = number1 + number2;
+    console.log(sum);
+    res.status(200);
+    res.json({
+        result: sum
+    });
+};
 
-    add(req, res) {
-        let number1 = parseInt(req.query.num1);
-        let number2 = parseInt(req.query.num2);
-        let sum = number1 + number2;
-        console.log(sum);
-        res.status(200);
-        res.json({
-            result: sum
-        });
-    };
+function subtract(req, res) {
+    let number1 = parseInt(req.query.num1);
+    let number2 = parseInt(req.query.num2);
+    let sum = number1 - number2;
+    console.log(sum);
+    res.status(200);
+    res.json({
+        result: sum
+    });
+};
 
-    subtract(req, res) {
-        let number1 = parseInt(req.query.num1);
-        let number2 = parseInt(req.query.num2);
-        let sum = number1 - number2;
-        console.log(sum);
-        res.status(200);
-        res.json({
-            result: sum
-        });
-    };
+function multiply(req, res) {
+    let number1 = parseInt(req.query.num1);
+    let number2 = parseInt(req.query.num2);
+    let sum = number1 * number2;
+    console.log(sum);
+    res.status(200);
+    res.json({
+        result: sum
+    });
+};
 
-    multiply(req, res) {
-        let number1 = parseInt(req.query.num1);
-        let number2 = parseInt(req.query.num2);
-        let sum = number1 / number2;
-        console.log(sum);
-        res.status(200);
-        res.json({
-            result: sum
-        });
-    };
+function divide(req, res) {
+    let number1 = parseInt(req.query.num1);
+    let number2 = parseInt(req.query.num2);
+    let sum = number1 / number2;
+    console.log(sum);
+    res.status(200);
+    res.json({
+        result: sum
+    });
+};
 
-    divide(req, res) {
-        let number1 = parseInt(req.query.num1);
-        let number2 = parseInt(req.query.num2);
-        let sum = number1 * number2;
-        console.log(sum);
-        res.status(200);
-        res.json({
-            result: sum
-        });
-    };
-}
-
-module.exports = calculatorController;
+module.exports = { add, subtract, multiply, divide };
