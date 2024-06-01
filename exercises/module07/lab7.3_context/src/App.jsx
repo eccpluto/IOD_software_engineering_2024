7a019afcb20bab471fe96d03615ff07a7c0cd67e
eccpluto <alleyes.payday591@passmail.net>
2024-06-01 15:13:16 +1200

    complete lab 7.3 by integrating with BitcoinRates component

--- exercises/module07/lab7.3_context/src/App.jsx
+++ exercises/module07/lab7.3_context/src/App.jsx
@@ -3,17 +3,17 @@ import './App.css'
 
 // access the mood provider from App.jsx
 import { MoodProvider } from './context/MoodContext'
-import EmojiToggler from './components/EmojiToggler'
+import BitcoinRates from './components/BitcoinRates'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    <h1>Module 7 Exercise 3</h1>
-    <MoodProvider>
-      <EmojiToggler></EmojiToggler>
-    </MoodProvider>
+      <h1>Module 7 Exercise 3</h1>
+      <MoodProvider>
+        <BitcoinRates></BitcoinRates>
+      </MoodProvider>
     </>
   )
 }
