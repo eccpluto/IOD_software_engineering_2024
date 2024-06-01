31979ed664b38ef251becc11769c833db69f15ad
eccpluto <alleyes.payday591@passmail.net>
2024-06-01 20:32:34 +1200

    updated login form to use MUI

--- exercises/module07/lab7.5_routing-mui-update/src/components/LoginForm.jsx
+++ exercises/module07/lab7.5_routing-mui-update/src/components/LoginForm.jsx
@@ -1,6 +1,21 @@
 import { useState } from "react";
 import { useUserContext } from "../context/UserContext";
 
+// mui
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+// import FormControl from '@mui/material/FormControl'
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+
 export default function LoginForm() {
 
     // component is stateful
@@ -41,15 +56,85 @@ export default function LoginForm() {
     );
 
     return (
-        <div className="LoginForm componentBox">
-            <label>Email: {/* form inputs with similar props */}
-                <input value={userEmail} onChange={handleUserEmailChange} />
-            </label>
-            <label>Password: {/* form inputs with similar props */}
-                <input value={userPassword} onChange={handleUserPasswordChange} />
-            </label>
-            <button onClick={handleSubmit}>Login</button>
-            <p>{submitResult}</p>
-        </div>
-    )
+        // <div className="LoginForm componentBox">
+        //     <label>Email: {/* form inputs with similar props */}
+        //         <input value={userEmail} onChange={handleUserEmailChange} />
+        //     </label>
+        //     <label>Password: {/* form inputs with similar props */}
+        //         <input value={userPassword} onChange={handleUserPasswordChange} />
+        //     </label>
+        //     <button onClick={handleSubmit}>Login</button>
+        //     <p>{submitResult}</p>
+        // </div>
+        <Container component="main" maxWidth="xs">
+            <CssBaseline />
+            <Box
+                sx={{
+                    marginTop: 8,
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                }}
+            >
+                <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+                    <LockOutlinedIcon />
+                </Avatar>
+                <Typography component="h1" variant="h5">
+                    Sign in
+                </Typography>
+                {submitResult}
+                <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+                    <TextField
+                        value={userEmail}
+                        margin="normal"
+                        required
+                        fullWidth
+                        id="email"
+                        label="Email Address"
+                        name="email"
+                        autoComplete="email"
+                        autoFocus
+                        onChange={handleUserEmailChange}
+                    />
+                    <TextField
+                        value={userPassword}
+                        margin="normal"
+                        required
+                        fullWidth
+                        name="password"
+                        label="Password"
+                        type="password"
+                        id="password"
+                        autoComplete="current-password"
+                        onChange={handleUserPasswordChange}
+                    />
+                    <FormControlLabel
+                        control={<Checkbox value="remember" color="primary" />}
+                        label="Remember me"
+                    />
+
+                    <Button
+                        type="submit"
+                        fullWidth
+                        variant="contained"
+                        sx={{ mt: 3, mb: 2 }}
+                    >
+                        Sign In
+                    </Button>
+                    <Grid container>
+                        <Grid item xs>
+                            <Link href="#" variant="body2">
+                                Forgot password?
+                            </Link>
+                        </Grid>
+                        <Grid item>
+                            <Link href="#" variant="body2">
+                                {"Don't have an account? Sign Up"}
+                            </Link>
+                        </Grid>
+                    </Grid>
+                </Box>
+            </Box>
+        </Container>
+    );
 }
