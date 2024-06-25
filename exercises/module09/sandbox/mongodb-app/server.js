947d90b675dd80be601386f92aabc72573e11db2
eccpluto <alleyes.payday591@passmail.net>
2024-06-25 21:41:59 +1200

    more informative loggin messages

--- exercises/module09/sandbox/mongodb-app/server.js
+++ exercises/module09/sandbox/mongodb-app/server.js
@@ -14,5 +14,5 @@ app.get("/", (req, res) => {
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is listening on port ${PORT}`);
 });
