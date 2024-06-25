17913248b1770b88a61b792d1bd364d6d93e0ba3
eccpluto <alleyes.payday591@passmail.net>
2024-06-25 21:57:15 +1200

    commented out controllers dependency which is not yet implemented

--- exercises/module09/sandbox/mongodb-app/routes/userRoutes.js
+++ exercises/module09/sandbox/mongodb-app/routes/userRoutes.js
@@ -2,7 +2,7 @@ let express = require("express");
 
 let router = express.Router();
 
-let Controllers = require("../controllers"); // index.js
+// let Controllers = require("../controllers"); // index.js
 
 // Adds a GET route to return all users
 router.get('/', (req, res) => {
